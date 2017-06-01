94eb7bba543d838e621e57ddea66b70fbc8b858e
aritbott <aritbott@gmail.com>
2017-06-01 12:05:23 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'HtgPvG1Wuvb+eHE/Os8UsbfeCR+RPRRrY60A+73HeC1ySEVWHUcwB3IdLu6B6Tyh/mF5hBh/puk0m1tcpKRKdxZW0CX/gtr1lqIps/LOQfQ7TGcTprx8117SBmeKUZtT9SIwf82P89S4z5NHzh5ZzwdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
